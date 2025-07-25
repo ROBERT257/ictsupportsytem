5d853b181261358e2dbd2fc20b0492426748a03c
ROBERT257 <robertochieng257@gmail.com>
2025-07-25 12:58:20 +0300

    Update DB connection to Railway

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$dbname = 'ictsupportsytem';
+$host = getenv('DB_HOST') ?: 'localhost';
+$user = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: '';
+$database = getenv('DB_NAME') ?: 'ictsupportsytem';
+$port = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $password, $database, $port);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
